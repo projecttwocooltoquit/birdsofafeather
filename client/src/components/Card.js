499136a9153ebe3202d14041aade54fb7853c532
Emily Rendleman <emilyrendleman@gmail.com>
2021-08-31 21:30:40 -0500

    added unsplash bird images to cards

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+const API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY;
 
 const Card = (props) => {
+  const [imageSrc, setImageSrc] = useState();
+
+  fetch(
+    `https://api.unsplash.com/search/photos?page=2&query=${props.comName}&per_page=1`,
+    {
+      headers: {
+        Authorization: `Client-ID ${API_KEY}`,
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setImageSrc(data.results[0].urls.full);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   return (
     <div className="card" style={{ width: 18 + "rem" }}>
-      <img className="card-img-top" src="..." alt="Card image cap" />
+      <img className="card-img-top" src={imageSrc} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">{props.comName}</h5>
         <p className="card-text">{props.sciName}</p>
