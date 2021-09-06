44dd855d6e17903839a9372a43991f39156c4d32
Emily Rendleman <emilyrendleman@gmail.com>
2021-09-06 10:45:06 -0500

    added infowindow to markers

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -1,5 +1,6 @@
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 import React, { useState, useEffect } from "react";
+import Spiderfy from "./Spiderfy";
 
 const MapContainer = (props) => {
   const mapStyles = {
@@ -12,29 +13,45 @@ const MapContainer = (props) => {
 
   // state to contain coordinates to center the map
   const [coordinates, setCoordinates] = useState({ lat: 10, lng: 10 });
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const [clickedMarker, setClickedMarker] = useState({ lat: 10, lng: 10 });
+  const [clickedBird, setClickedBird] = useState("");
 
-  // listens for changes in props, sets the coordinates of the center to whatever the center coordinate that is passed into the component
+  // sets the coordinates of the center to whatever the center coordinate that is passed into the component
   useEffect(() => {
     if (props.center) {
       setCoordinates({ lat: props.center.lat, lng: props.center.lng });
     }
   }, [props.center]);
 
+  const onMarkerClick = (e) => {
+    setClickedBird(e.value);
+    setClickedMarker(e.position);
+    setShowingInfoWindow(true);
+  };
+
   // only renders markers if props are defined, otherwise renders map with no markers
   if (props.locations) {
     return (
       <Map
         google={props.google}
-        zoom={8}
+        zoom={10}
         style={mapStyles}
         center={coordinates}
       >
         {props.locations.map((location, index) => (
           <Marker
             key={index}
+            value={location.comName}
             position={{ lat: location.lat, lng: location.lng }}
-          />
+            onClick={(e) => onMarkerClick(e)}
+          ></Marker>
         ))}
+        <InfoWindow visible={showingInfoWindow} position={clickedMarker}>
+          <div>
+            <h4>{clickedBird}</h4>
+          </div>
+        </InfoWindow>
       </Map>
     );
   } else {
@@ -51,5 +68,5 @@ const MapContainer = (props) => {
 };
 
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyAs3k9TKjBGM8LpOITw_YbAXNMAIXAvU_A",
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
 })(MapContainer);
