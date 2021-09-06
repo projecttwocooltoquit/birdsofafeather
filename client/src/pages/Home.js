8417971d2810a4ad25b5b44767baffbdeeb6e824
Emily Rendleman <emilyrendleman@gmail.com>
2021-09-06 16:00:59 -0500

    fleshed out profile page, added css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -319,7 +319,15 @@ const Home = () => {
       </video>
       <div className="flexWrap">
         <section className="menuPosition">
-          <p className="lead">Select a state and county to begin.</p>
+          <h4>Select a state and county to begin.</h4>
+          <p>
+            To add a bird you've seen to your profile, click "Add to Spotted
+            List"
+          </p>
+          <p>
+            If you see a bird you'd like to keep an eye out for, you can add
+            that bird to your Watch List by clicking "Add to Watch List"
+          </p>
           <div className="container d-flex">
             <select
               id="state-dropdwn"
